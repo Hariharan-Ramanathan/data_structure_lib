55e5a0267876bdc08b290df37316d72bb0ba4d37
Hariharan-Ramanathan <hariharan.r1999@gmail.com>
2020-07-29 16:07:26 +0530

    bstPopComment

--- bst/sourceFiles/pop.c
+++ bst/sourceFiles/pop.c
@@ -2,43 +2,39 @@ void int_bst_pop(int_bst_node** root,int data){
 
 	if(*root==NULL)
 		return ;
+	
 	if(data < (*root)->data)
-		int_bst_pop(&((*root)->left),data);			
+		int_bst_pop(&((*root)->left),data);			//search for the node to be deleted
 	else if(data > (*root)->data)
-		int_bst_pop(&((*root)->right),data);
+		int_bst_pop(&((*root)->right),data);		//search for the node to be deleted
 	
-	else{
-		if((*root)->left ==NULL)
-			(*root)=(*root)->right;
-		else if((*root)->right==NULL)
-			(*root)=(*root)->left;
-		else{
-
-		if((*root)->left!=NULL){
-			int_bst_node* temp= *root;
-			(temp)=(temp)->left;
+	else if((*root)->data == data){					//after reaching the node to be deleted
+		if((*root)->left ==NULL ){					//if that node's left is NULL then that node shold be replaced by its right node
+			int_bst_node* temp = (*root)->right;
+			(*root)=temp;
+			}
 		
-			while((temp)->right != NULL)
-				(temp)=(temp)->right;
-
-			(*root)->data=temp->data;
-			int_bst_pop(&(*root)->left,temp->data);			
+		else if((*root)->right==NULL){
+			int_bst_node* temp = (*root)->left;		//if that node's right is NULL then that node should be replaced by its left node
+			(*root)=temp;
+			}
 		
-	}
-		else{
-			int_bst_node* temp=*root;
-			(temp)=(temp)->right;
-	
+		else{										//if a node having both left and right child need to be deleted , that node data should be either replaced by largest data in left tree of smallest data in right tree
+		int val = 0;
+		int_bst_node* temp = *root;
+													// here i took the smallest data of right tree	
+			(temp)=(temp)->right;					// you can also take largest data of left tree , both will maintain the bst
 			while((temp)->left !=NULL)
 				(temp)=(temp)->left;
-	
-			(*root)->data=temp->data;
-			int_bst_pop(&(*root)->right,temp->data);
+			val = (temp)->data;
+			(*root)->data = val;					//coping the smallest data of right tree to the node to be deleted
+		  int_bst_pop(&((*root)->right),val);		// after copying the node whose value is copied from shouls be removes/deleted so recurse
+			
 			}
 		}
 
 	}
-}
+
 
 void double_bst_pop(double_bst_node** root,double data){
 
@@ -56,7 +52,6 @@ void double_bst_pop(double_bst_node** root,double data){
 			(*root)=(*root)->left;
 		else{
 
-		if((*root)->left!=NULL){
 			double_bst_node* temp= *root;
 			(temp)=(temp)->left;
 		
@@ -65,18 +60,6 @@ void double_bst_pop(double_bst_node** root,double data){
 
 			(*root)->data=temp->data;
 			double_bst_pop(&(*root)->left,temp->data);			
-		
-	}
-		else{
-			double_bst_node* temp=*root;
-			(temp)=(temp)->right;
-	
-			while((temp)->left !=NULL)
-				(temp)=(temp)->left;
-	
-			(*root)->data=temp->data;
-			double_bst_pop(&(*root)->right,temp->data);
-			}
 		}
 
 	}
@@ -96,8 +79,6 @@ void float_bst_pop(float_bst_node** root,float data){
 		else if((*root)->right==NULL)
 			(*root)=(*root)->left;
 		else{
-
-		if((*root)->left!=NULL){
 			float_bst_node* temp= *root;
 			(temp)=(temp)->left;
 		
@@ -106,18 +87,7 @@ void float_bst_pop(float_bst_node** root,float data){
 
 			(*root)->data=temp->data;
 			float_bst_pop(&(*root)->left,temp->data);			
-		
-	}
-		else{
-			float_bst_node* temp=*root;
-			(temp)=(temp)->right;
 	
-			while((temp)->left !=NULL)
-				(temp)=(temp)->left;
-	
-			(*root)->data=temp->data;
-			float_bst_pop(&(*root)->right,temp->data);
-			}
 		}
 
 	}
--- main/bstmain.c
+++ main/bstmain.c
@@ -8,15 +8,16 @@ int main(void){
 struct int_bstf l= import_int_bstf;
 int_bst_node* bst=l.create();
 
-	l.push(&bst,5);
-	l.push(&bst,3);
-	l.push(&bst,6);
-	l.push(&bst,1);
-	l.push(&bst,7);
-	l.push(&bst,4);
-	l.pop(&bst,5);
-	l.pop(&bst,3);
-	l.pop(&bst,7);
+	l.push(&bst, 5);
+	l.push(&bst, 3);
+	l.push(&bst, 10);
+	l.push(&bst, 2);
+	l.push(&bst, 4);
+	l.push(&bst, 0);
+	l.push(&bst, 1);
+	l.pop(&bst, 0);
+	//l.pop(&bst,3);
+	//l.pop(&bst,7);
 	l.preorder(bst);
 	printf("\n");
 return 0;
